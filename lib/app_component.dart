b5cf5ca3b7a1145a16f6390c4fa8dadd177f4e75
hjyanghj <hjyanghj@gmail.com>
2018-08-27 22:07:54 +0800

    修改成教學課程的簡單初始狀況

--- lib/app_component.dart
+++ lib/app_component.dart
@@ -1,16 +1,8 @@
 import 'package:angular/angular.dart';
-
-import 'src/todo_list/todo_list_component.dart';
-
-// AngularDart info: https://webdev.dartlang.org/angular
-// Components info: https://webdev.dartlang.org/components
-
 @Component(
   selector: 'my-app',
-  styleUrls: ['app_component.css'],
-  templateUrl: 'app_component.html',
-  directives: [TodoListComponent],
+  template: '<h1>Hello {{name}}</h1>',
 )
 class AppComponent {
-  // Nothing here yet. All logic is in TodoListComponent.
+  var name = 'Angular';
 }
